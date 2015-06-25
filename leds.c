1357b52ca62d377496daba79ce49a64d68aa8f72
George Louthan <duplico@dupli.co>
2015-06-25 00:06:34 -0500

    Refactor out a lot of LED code. Get 7-bit mode working.

--- leds.c
+++ leds.c
@@ -8,66 +8,140 @@
 #include "leds.h"
 #include <string.h>
 
-#define DISPLAY_OFF 	0
-#define DISPLAY_ON  	1
+/*
+ *   LED controller (TLC5948A)
+ *        (write on rise, change on fall,
+ *         clock inactive low, MSB first)
+ *        eUSCI_A0 - LEDs  (shared)
+ *        somi, miso, clk
+ *        GSCLK     P1.2 (timer TA1.1)
+ *        LAT       P1.4
+ */
 
-volatile uint8_t f_time_loop = 0;
+volatile uint8_t f_time_loop = 0; // TODO
 
-#define DISP_MODE_ANIM  -1
-#define DISP_MODE_SCROLL 0
-#define DISP_MODE_TEXT   1
+// NB: Make sure that the MSB here is never 0b1:
+const uint8_t dot_correct_settings[] = {
+		0x7f,	// R
+		0x0f,	// G
+		0x0f,	// B
+		0x7f,	// R
+		0x0f,	// G
+		0x0f,	// B
+		0x7f,	// R
+		0x0f,	// G
+		0x0f,	// B
+		0x7f,	// R
+		0x0f,	// G
+		0x0f,	// B
+		0x7f,	// R
+		0x0f,	// G
+		0x0f,	// B
+		0x7f
+};
 
-void led_init() {
 
-	// Setup LED module pins //////////////////////////////////////////////////
-	// LED_PORT.LED_DATA, LED_CLOCK, LED_LATCH, LED_BLANK
-	P1DIR |= LED_DATA + LED_CLOCK + LED_LATCH + LED_BLANK;
+void tlc_set_gs() {
+    while (!EUSCI_A_SPI_getInterruptStatus(EUSCI_A0_BASE,
+            EUSCI_A_SPI_TRANSMIT_INTERRUPT));
 
-	// Shift register input from LED controllers:
-	// LEDPORT.PIN6 as input:
-	P1DIR &= ~BIT6;
+    // We need a 0 to show it's GS:
+    usci_a_send(EUSCI_A0_BASE, 0x00);
+    // Now the GS data itself. There are 16 channels; MSB first!
+    for (uint8_t channel=16; channel; channel--) {
+        // ...with 16 bits each:
+        usci_a_send(EUSCI_A0_BASE, (channel % 3) ? 0x0f : 0x0f); // MSByte
+        usci_a_send(EUSCI_A0_BASE, 0x00); // LSByte
+    }
+
+    // LATCH:
+    GPIO_pulse(GPIO_PORT_P1, GPIO_PIN4);
+}
+
+void tlc_set_fun() {
+    while (!EUSCI_A_SPI_getInterruptStatus(EUSCI_A0_BASE,
+            EUSCI_A_SPI_TRANSMIT_INTERRUPT));
+
+    usci_a_send(EUSCI_A0_BASE, 0x01); // 1 for Function
+
+    for (uint8_t i=0; i<14; i++) {
+        usci_a_send(EUSCI_A0_BASE, 0x00);
+    }
+
+    usci_a_send(EUSCI_A0_BASE, 0x00); // LSB of this is PSM(D2)
+
+    // B135 / PSM(D1)
+    // B134 / PSM(D0)
+    // B133 / OLDENA
+    // B132 / IDMCUR(D1)
+    // B131 / IDMCUR(D0)
+    // B130 / IDMRPT(D0)
+    // B129 / IDMENA
+    // B128 / LATTMG(D1)
+
+    usci_a_send(EUSCI_A0_BASE, 0x01);
+
+    // B127 / LATTMG(D0)
+    // B126 / LSDVLT(D1)
+    // B125 / LSDVLT(D0)
+    // B124 / LODVLT(D1)
+    // B123 / LODVLT(D0)
+    // B122 / ESPWM
+    // B121 / TMGRST
+    // B120 / DSPRPT
+
+    usci_a_send(EUSCI_A0_BASE, 0x85);
+
+    // B119 / BLANK
+    // MSB is BLANK; remainder are BC:
+    usci_a_send(EUSCI_A0_BASE, 0x08);
+
+
+    UCA0CTLW0 |= UCSWRST;
+    UCA0CTLW0 |= UC7BIT;
+    UCA0CTLW0 &= ~UCSWRST;
+
+    // 5 dot-correct 3x 7-tets for the RGBs:
+    for (uint8_t i=0; i<5; i++) {
+    	usci_a_send(EUSCI_A0_BASE, 0x4F); // RED
+    	usci_a_send(EUSCI_A0_BASE, 0x7F); // BLUE
+    	usci_a_send(EUSCI_A0_BASE, 0x7F); // GREEN
+    }
+
+    // Plus one for the TX light:
+    usci_a_send(EUSCI_A0_BASE, 0x7F);
+
+    // 16 dot-correct 7-tets in 14 octets:
+//    for (uint8_t i=0; i<14; i++) {
+//        usci_a_send(EUSCI_A0_BASE, 0xFF);
+//    }
+
+    // LATCH:
+    GPIO_pulse(GPIO_PORT_P1, GPIO_PIN4);
+
+    UCA0CTLW0 |= UCSWRST;
+    UCA0CTLW0 &= ~UC7BIT;
+    UCA0CTLW0 &= ~UCSWRST;
+}
+
+
+
+
+
+
+void init_leds() {
+
+    UCA0CTLW0 |= UCSWRST;
+    UCA0CTLW0 &= ~UC7BIT;
+    UCA0CTLW0 &= ~UCSWRST;
+
+    tlc_set_gs();
+    tlc_set_fun();
 }
 
 void led_enable(uint16_t duty_cycle) {
-//	GPIO_setAsPeripheralModuleFunctionOutputPin(LED_PORT, LED_BLANK);
-//	P1SEL |= LED_BLANK; // TODO
-
-//	TIMER_A_generatePWM(
-//		TIMER_A0_BASE,
-//		TIMER_A_CLOCKSOURCE_ACLK,
-//		TIMER_A_CLOCKSOURCE_DIVIDER_1,
-//		LED_PERIOD, // period
-//		TIMER_A_CAPTURECOMPARE_REGISTER_2,
-//		TIMER_A_OUTPUTMODE_RESET_SET,
-//		LED_PERIOD - duty_cycle // duty cycle
-//	);
-
-	TA0CTL &= ~( TIMER_A_CLOCKSOURCE_INVERTED_EXTERNAL_TXCLK +
-	           TIMER_A_UPDOWN_MODE + TIMER_A_DO_CLEAR +
-	           TIMER_A_TAIE_INTERRUPT_ENABLE
-	           );
-
-	TA0CTL |= ( TIMER_A_CLOCKSOURCE_ACLK +
-				TIMER_A_UP_MODE +
-				TIMER_A_DO_CLEAR
-			  );
-
-	TA0CCR0 = LED_PERIOD;
-
-	TA0CCTL0 &= ~(TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE +
-	          	  TIMER_A_OUTPUTMODE_RESET_SET
-	          	 );
-
-	TA0CCTL2 |= TIMER_A_OUTPUTMODE_RESET_SET;
-	TA0CCR2 = LED_PERIOD - duty_cycle;
-	TA1CTL |= TIMER_A_UP_MODE;
-
 }
 
 void led_disable( void )
 {
-//	GPIO_setAsOutputPin(
-//			LED_PORT,
-//			LED_BLANK
-//	); // TODO
 }
--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 #include "img.h"
 #include "qc12.h"
 #include "radio.h"
+#include "leds.h"
 
 /*
  * Peripherals:
@@ -29,16 +30,6 @@
  */
 
 /*
- *   LED controller (TLC5948A)
- *        (write on rise, change on fall,
- *         clock inactive low, MSB first)
- *        eUSCI_A0 - LEDs  (shared)
- *        somi, miso, clk
- *        GSCLK     P1.2 (timer TA1.1)
- *        LAT       P1.4
- */
-
-/*
  *   Flash chip (S25FL216K)
  *        (write on rise, change on fall,
  *         CS active low, clock inactive low, MSB first)
@@ -47,6 +38,7 @@
  *        CS        P1.1
  *        WP        P3.0
  *        HOLD      P1.0
+ *        TODO: DMA: https://e2e.ti.com/support/microcontrollers/msp430/f/166/t/235583
  */
 
 /*
@@ -78,28 +70,6 @@
 
 tContext g_sContext;
 
-
-void tlc_bit(uint8_t bit) {
-     // CLK should be low.
-     P1OUT &= ~BIT5;
-     // Set it to the bit:
-     if (bit) {
-          P2OUT |= BIT0;
-     } else {
-          P2OUT &= ~BIT0;
-     }
-     // Pull CLK high
-     P1OUT |= BIT5;
-}
-
-uint8_t tlc_in_bit() {
-     // CLK should be low.
-     P1OUT &= ~BIT5;
-     // Pulse CLK high to clock it in.
-     P1OUT |= BIT5;
-     return P2IN & BIT1;
-}
-
 void usci_a_send(uint16_t base, uint8_t data) {
 	while (EUSCI_A_SPI_isBusy(base));
 	while (!EUSCI_A_SPI_getInterruptStatus(base,
@@ -109,71 +79,6 @@ void usci_a_send(uint16_t base, uint8_t data) {
 			EUSCI_A_SPI_TRANSMIT_INTERRUPT));
 	while (EUSCI_A_SPI_isBusy(base));
 }
-
-void tlc_set_gs() {
-    while (!EUSCI_A_SPI_getInterruptStatus(EUSCI_A0_BASE,
-            EUSCI_A_SPI_TRANSMIT_INTERRUPT));
-
-    // We need a 0 to show it's GS:
-    usci_a_send(EUSCI_A0_BASE, 0x00);
-    // Now the GS data itself. There are 16 channels;
-    for (uint8_t channel=0; channel<16; channel++) {
-        // ...with 16 bits each:
-        usci_a_send(EUSCI_A0_BASE, 0x00); // MSByte
-        usci_a_send(EUSCI_A0_BASE, (channel % 3) ? 0xff : 0x00); // LSByte
-    }
-
-    // LATCH:
-    GPIO_pulse(GPIO_PORT_P1, GPIO_PIN4);
-}
-
-void tlc_set_fun() {
-    while (!EUSCI_A_SPI_getInterruptStatus(EUSCI_A0_BASE,
-            EUSCI_A_SPI_TRANSMIT_INTERRUPT));
-
-    usci_a_send(EUSCI_A0_BASE, 0x01); // 1 for Function
-
-    for (uint8_t i=0; i<14; i++) {
-        usci_a_send(EUSCI_A0_BASE, 0x00);
-    }
-
-    usci_a_send(EUSCI_A0_BASE, 0x00); // LSB of this is PSM(D2)
-
-    // B135 / PSM(D1)
-    // B134 / PSM(D0)
-    // B133 / OLDENA
-    // B132 / IDMCUR(D1)
-    // B131 / IDMCUR(D0)
-    // B130 / IDMRPT(D0)
-    // B129 / IDMENA
-    // B128 / LATTMG(D1)
-
-    usci_a_send(EUSCI_A0_BASE, 0x01);
-
-    // B127 / LATTMG(D0)
-    // B126 / LSDVLT(D1)
-    // B125 / LSDVLT(D0)
-    // B124 / LODVLT(D1)
-    // B123 / LODVLT(D0)
-    // B122 / ESPWM
-    // B121 / TMGRST
-    // B120 / DSPRPT
-
-    usci_a_send(EUSCI_A0_BASE, 0x85);
-
-    // B119 / BLANK
-    // MSB is BLANK; remainder are BC:
-    usci_a_send(EUSCI_A0_BASE, 0x7F);
-
-    for (uint8_t i=0; i<14; i++) {
-        // 16 DC 7-tets in 14 octets/bytes:
-        usci_a_send(EUSCI_A0_BASE, 0xFF);
-    }
-
-    // LATCH:
-    GPIO_pulse(GPIO_PORT_P1, GPIO_PIN4);
-}
-
 
 void init() {
     Grace_init(); // Activate Grace-generated configuration
@@ -191,6 +96,8 @@ void init() {
      */
     init_radio();
 
+    init_leds();
+
     /*
      *   OLED (OLED_0.96)
      *        (write on rise, change on fall,
@@ -204,13 +111,13 @@ void init() {
     GrContextInit(&g_sContext, &g_sqc12_oled);
     GrContextBackgroundSet(&g_sContext, ClrBlack);
     GrContextForegroundSet(&g_sContext, ClrWhite);
-    GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
+    GrContextFontSet(&g_sContext, &g_sFontCmsc12); // &g_sFontFixed6x8);
     GrClearDisplay(&g_sContext);
     GrImageDraw(&g_sContext, &standing_1, 0, 32);
-    GrStringDraw(&g_sContext, "4 DUPLiCO", -1, 0, 0, 1);
-    GrCircleDraw(&g_sContext, 2, 3, 6);
+    GrStringDraw(&g_sContext, "DUPLiCO", -1, 0, 0, 1);
+//    GrCircleDraw(&g_sContext, 2, 3, 6);
     GrStringDraw(&g_sContext, "  the gay", -1, 0, 9, 0);
-    GrStringDraw(&g_sContext, "<  Play! >", -1, 0, 120, 1);
+    GrStringDraw(&g_sContext, "<  Play! >", -1, 0, 116, 1);
     GrFlush(&g_sContext);
 }
 
@@ -223,9 +130,6 @@ int main(void)
     init();
     // TODO:
     //    post();
-
-    tlc_set_gs();
-    tlc_set_fun();
 
     // TESTING THE FLASH:
 
