694f7d2a8edf234c1ff98830265754a84c7c6e17
George Louthan <georgerlouth@nthefourth.com>
2015-07-28 21:20:39 -0500

    Increase ambient brightness.

--- leds.c
+++ leds.c
@@ -300,12 +300,12 @@ uint8_t ring_fade_index = 0;
 uint8_t ring_fade_steps = 0;
 
 const rgbcolor_t color_off = {0, 0, 0};
-const rgbcolor_t mood_green = { 0x0000, 0x0f00, 0x0000};
-const rgbcolor_t mood_yellow   = { 0x0800, 0x0800, 0x0000}; // To account for roundoff error.
-const rgbcolor_t mood_red   = { 0x0f00, 0x0000, 0x0000};
+const rgbcolor_t mood_green = { 0x0000, 0x2000, 0x0000};
+const rgbcolor_t mood_yellow   = { 0x1000, 0x1100, 0x0000}; // To account for roundoff error.
+const rgbcolor_t mood_red   = { 0x2000, 0x0000, 0x0000};
 const rgbdelta_t mood_step = {
-        (-0x0f00) / 100,
-        (0x0f00) / 100,
+        (-0x2000) / 100,
+        (0x2000) / 100,
         (0x0000) / 100
 };
 
