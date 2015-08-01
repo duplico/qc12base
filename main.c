17cc8bb6cc16ad2a5bbe513b48ba5500d51924b1
George Louthan <duplico@dupli.co>
2015-08-01 13:11:07 -0500

    Completely remove the dead flash code, resolving #60.

--- main.c
+++ main.c
@@ -15,7 +15,6 @@
 #include <radio.h>
 #include <leds.h>
 #include <oled.h>
-#include <flash.h>
 
 // Interrupt flags:
 volatile uint8_t f_bl = 0;
@@ -450,18 +449,10 @@ void post() {
     uint8_t led_error = tlc_test_loopback(0b10101010);
     led_error = led_error || tlc_test_loopback(0b01010101);
 
-    // Check the flash chip.
     tlc_set_fun();
-    EUSCI_A_SPI_disableInterrupt(EUSCI_A0_BASE, EUSCI_A_SPI_RECEIVE_INTERRUPT);
-    EUSCI_A_SPI_disableInterrupt(EUSCI_A0_BASE, EUSCI_A_SPI_TRANSMIT_INTERRUPT);
-    uint8_t flash_error = flash_post();
-    EUSCI_A_SPI_clearInterrupt(EUSCI_A0_BASE, EUSCI_A_SPI_RECEIVE_INTERRUPT);
-    EUSCI_A_SPI_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_SPI_RECEIVE_INTERRUPT);
-    EUSCI_A_SPI_clearInterrupt(EUSCI_A0_BASE, EUSCI_A_SPI_TRANSMIT_INTERRUPT);
-    EUSCI_A_SPI_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_SPI_TRANSMIT_INTERRUPT);
 
     // If we detected no errors, we're done here.
-    if (!(flash_error || led_error || crystal_error))
+    if (!(led_error || crystal_error))
         return;
 
     // Otherwise, show those errors and then delay for a bit so we can
@@ -479,11 +470,6 @@ void post() {
 
     if (led_error) {
         GrStringDraw(&g_sContext, "Err: LED!", -1, 0, print_loc, 0);
-        print_loc += 12;
-    }
-
-    if (flash_error) {
-        GrStringDraw(&g_sContext, "Err: Mem!", -1, 0, print_loc, 0);
         print_loc += 12;
     }
 
